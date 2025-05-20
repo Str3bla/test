8e2a4f3710455c27fe6fec66d78b995bf121573e
Str3bla <reportingology@gmail.com>
2025-05-19 20:10:14 -0400

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -72,3 +72,16 @@ font="monospace"
 st.write("")
 st.write("")
 st.write("")
+
+st.title('st.progress')
+
+with st.expander('About this app'):
+     st.write('You can now display the progress of your calculations in a Streamlit app with the `st.progress` command.')
+
+my_bar = st.progress(0)
+
+for percent_complete in range(100):
+     time.sleep(0.05)
+     my_bar.progress(percent_complete + 1)
+
+st.balloons()
